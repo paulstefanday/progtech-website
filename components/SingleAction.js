9a67b4075263aa21f9ef2cb44b722d18d06ff795
Paul <paul@paulday.com.au>
2020-08-19 18:32:34 +1000

    added bios

--- components/SingleAction.js
+++ components/SingleAction.js
@@ -11,11 +11,15 @@ const SingleAction = () => (
       </Box>
       <Box p={[4, 5]} width={[1, 2 / 3]} textAlign="center" bg="test2">
         <Container justifyContent="center">
-          <Heading variant="highlight.purple">Sector Consultation</Heading>
+          <Heading variant="highlight.purple">Launch Event</Heading>
+          <p><Text variant="p">
+         
+          We'll be hearing from Edie Shepherd (ProgTech, Original Power) on the tech support BIPOC activists need, Josh Nussbaum (Movement Cooperative US) on what US organisations have achieved through teaming up on tech, and the key learnings from the biggest consultation on where progressive tech is at in Australia.
+</Text>
           <Text variant="p">
-            We've wrapped on the formal consultations on progressive organisations and our tech tools - and analysts are putting together anonymised and aggregated analysis to be released Thursday August 13th 2020 so we can all see the state of progressive tech in Australia. Sign up for updates above to keep in the loop! If you'd like to share how your organisation is using tech, please do still fill out the survey - your input will support ProgTech in better identifying our common challenges, and the highest impact interventions we can collectively make with precision.
-          </Text>
-          <Link target="_blank" href="https://form.typeform.com/to/C8Sthe"><Button variant="primary">Take the survey now</Button></Link>
+Join us at Thursday, Aug 27th at 11am AEST / 10.30 ACST / 9 AWST!
+          </Text></p>
+          <Link target="_blank" href="https://form.typeform.com/to/ZAU0MgR5"><Button variant="primary">Count me in</Button></Link>
         </Container>
       </Box>
     </>
--- pages/team.js
+++ pages/team.js
@@ -10,25 +10,146 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-const colors = [
-  'purple', 'orange', 'green', 'pink'
-]
+const colors = ["purple", "orange", "green", "pink"];
 
 const list = [
-  { url: "/images/team-clo.jpg", name: "Clodagh Schofield", bio: "" },
-  { url: "/images/team-harry.jpg", name: "Harry Cossar-Gilbert", bio: "" },
-  { url: "/images/team-moira.jpg", name: "Moira Cully", bio: "" },
-  { url: "/images/team-edie.jpg", name: "Edie Shepherd", bio: "" },
-  { url: "/images/team-paul.jpg", name: "Paul Day", bio: "" },
-  { url: "/images/team-sara.jpg", name: "Sara Smylie", bio: "" },
+  {
+    url: "/images/team-clo.jpg",
+    name: "Clodagh Schofield",
+    bio: (
+      <>
+        <p>
+          Clodagh Schofield is a highly enthusiastic nerd with a passion for
+          getting the tech we need to scale our movements into the hands of
+          every progressive activist that needs it.
+        </p>
+        <p>
+          She's currently excited about building a network to share tech skills
+          & tools across our movements. She works as the AYCC's Tech & Data
+          Manager, and on the team at the Progressive Tech Network.
+        </p>
+      </>
+    ),
+  },
+  {
+    url: "/images/team-harry.jpg",
+    name: "Harry Cossar-Gilbert",
+    bio: (
+      <>
+        <p>
+          Harry is a progressive technologist and lawyer, with a background in
+          developing lightweight technological solutions for small to medium
+          campaigning organisations.
+        </p>
+        <p>
+          Harry leads finance, governance, and fundraising for ProgTech, and
+          brings both expertise and passion for climate justice to the team.
+        </p>
+      </>
+    ),
+  },
+  {
+    url: "/images/team-moira.jpg",
+    name: "Moira Cully",
+    bio: (
+      <>
+        <p>
+          Moira is a digital strategist and technologist specialising in
+          supporting unions in best practice digital recruitment and
+          campaigning. She leads one of the most effective technological
+          collaborations between Australian unions, Megaphone, facilitating
+          digital growth and campaign impact across the sector.
+        </p>
+        <p>
+          Moira brings expertise in network organising and using digital to
+          scale the union movement, and leads the training program and
+          institutional fundraising work of ProgTech.
+        </p>
+      </>
+    ),
+  },
+  {
+    url: "/images/team-edie.jpg",
+    name: "Edie Shepherd",
+    bio: (
+      <>
+        <p>
+          Edie Shepherd is a proud Wiradjuri and Noongar woman and senior
+          organiser at Original Power. She has worked as a youth worker,
+          community organiser and campaigner within Aboriginal and Torres Strait
+          Islander communities, as well as in broader social and economic
+          justice spaces.
+        </p>
+        <p>
+          Before OP, Edie has spent 3 years working as an organiser in the trade
+          union movement, running Aboriginal and Torres Strait Islander
+          organising and political capacity building programs in Victoria.
+        </p>
+      </>
+    ),
+  },
+  {
+    url: "/images/team-paul.jpg",
+    name: "Paul Day",
+    bio: (
+      <>
+        <p>
+          Paul Day is a full stack developer specialising in progressive
+          technology in Australia. He brings his expertise in building
+          best-in-class tools that make digital fundraising accessible to
+          grassroots groups and small organisations through his work at Chuffed
+          and Raisely.
+        </p>
+        <p>
+          Paul focuses on identifying areas where shared challenges offer
+          potential for building shared solutions - alongside running solidarity
+          tech support and training. He's excited to grow the strong progressive
+          tech community to further support organisations in doing good work.
+        </p>
+      </>
+    ),
+  },
+  {
+    url: "/images/team-sara.jpg",
+    name: "Sara Smylie",
+    bio: (
+      <>
+        <p>
+          Sara is a National Coordinator of New Organising at United Workers
+          Union. United Workers Union members - including cleaners, early
+          childhood educators and care workers - are at the coalface of rising
+          inequality and changes to the nature of work, and are at the forefront
+          of fighting back.
+        </p>
+        <p>
+          {" "}
+          Sara trials new approaches to organising, using tech to allow workers
+          in disparate sites to connect, take action and build power
+          collectively.
+        </p>
+      </>
+    ),
+  },
 ];
 
 const Team = () => (
   <Flex width={[1]} flexWrap="wrap">
     {list.map(({ url, name, bio }, index) => (
-      <Box textAlign="center" width={[1, 1/2, 1 / 4]} minHeight={230} mb={5} sx={styles}>
-        <div style={{ width: "100%", height: "240px", overflow:"hidden"}}><Image src={url} mb={2} width="100%" /></div>
-        <br /><Heading variant={`highlight.${colors[getRandomInt(4)]}`}>{name}</Heading>
+      <Box
+        textAlign="center"
+        width={[1, 1 / 2, 1 / 3]}
+        minHeight={230}
+        mb={5}
+        sx={styles}
+      >
+        <div style={{ width: "100%", height: "240px", overflow: "hidden" }}>
+          <Image src={url} mb={2} width="100%" />
+        </div>
+        <br />
+        <Heading variant={`highlight.${colors[getRandomInt(4)]}`}>
+          {name}
+        </Heading>
+        <Box textAlign="left" p={3}>{bio}</Box>
       </Box>
     ))}
   </Flex>
