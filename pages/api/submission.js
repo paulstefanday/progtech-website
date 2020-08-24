5b109800648cb8102747dd1f0a39698ccf7757bc
Paul <paul@paulday.com.au>
2020-08-24 17:33:21 +1000

    added slack hooks back in

--- pages/api/submission.js
+++ pages/api/submission.js
@@ -1,14 +1,84 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
+import { IncomingWebhook } from "@slack/webhook";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 // POST /api/submission
-// Required fields in body: 
+// Required fields in body:
 export default async function handle(req, res) {
   const result = await prisma.submissions.create({
     data: {
       ...req.body,
     },
-  })
-  res.json(result)
+  });
+
+  const { form } = req.body;
+
+  // send to asana
+  if(form === "PARTNER") {
+    // 1/5744038961411:86ae70d2c493c275940f1069f44b61a2
+  }
+
+  // send to slack
+  if (form === "NEWSLETTER") {
+    const { email } = req.body;
+    if (email && email !== "") {
+      const url =
+        "https://hooks.slack.com/services/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";
+      const webhook = new IncomingWebhook(url);
+      await webhook.send({ text: `*New Newsletter Signup:* ${email}` });
+    }
+  } else if (form === "CONTACT") {
+    const {
+      email,
+      name,
+      data: { message },
+    } = req.body;
+    if (email && email !== "") {
+      const url =
+        "https://hooks.slack.com/services/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";
+      const webhook = new IncomingWebhook(url);
+      await webhook.send({
+        text: `*New Contact Form:* \n*Email:* ${email}\n*Name:* ${name}\n*Message:* ${message}`,
+      });
+    }
+  } else if (form === "PARTNER") {
+    const {
+      email,
+      name,
+      data: {
+        role,
+        canOffer,
+        keyBenefits,
+        organisation,
+        keyChallenges,
+        wishYouCouldDo,
+      },
+    } = req.body;
+    if (email && email !== "") {
+      const url =
+        "https://hooks.slack.com/services/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";
+      const webhook = new IncomingWebhook(url);
+      await webhook.send({
+        text: `*Email:* ${email}
+*Name:* ${name}
+*Organsation:* ${organisation}
+*Role:* ${role}
+
+*What are the key things your organisation has to offer and share with other progressive organisations and the network?* 
+${canOffer}
+
+*What are the key ways your organisation could benefit from collaboration with other organisations and the network?* 
+${keyBenefits}
+
+*What are the key challenges your organisation faces with tech and data?* 
+${keyChallenges}
+
+*What are the things you wish you could do?* 
+${wishYouCouldDo}`,
+      });
+    }
+  }
+
+  res.json(result);
 }
