a78a345b6c807e800c8223419f9081b8c4e1c26f
mkcully <38482720+mkcully@users.noreply.github.com>
2020-10-23 16:41:32 +1100

    Update team page
    Updating team page to reflect interim network board

--- pages/team.js
+++ pages/team.js
@@ -14,48 +14,41 @@ const colors = ["purple", "orange", "green", "pink"];
 
 const list = [
   {
-    url: "/images/team-clo.jpg",
-    name: "Clodagh Schofield",
+    url: "/images/team-sara.png",
+    name: "Sara Smylie",
     bio: (
       <>
         <p>
-          Clodagh Schofield is a highly enthusiastic nerd with a passion for
-          getting the tech we need to scale our movements into the hands of
-          every progressive activist that needs it.
+          Sara is a National Coordinator of New Organising at United Workers Union. United Workers Union members - including cleaners, early childhood educators and care workers - are at the coalface of rising inequality and changes to the nature of work, and are at the forefront of fighting back.
         </p>
         <p>
-          She's currently excited about building a network to share tech skills
-          & tools across our movements. She works as the AYCC's Tech & Data
-          Manager, and on the team at the Progressive Tech Network.
+          Sara trials new approaches to organising, using tech to allow workers in disparate sites to connect, take action and build power collectively.
         </p>
       </>
     ),
   },
   {
-    url: "/images/team-harry.jpg",
-    name: "Harry Cossar-Gilbert",
+    url: "/images/team-edie.png",
+    name: "Edie Shepherd",
     bio: (
       <>
         <p>
-          Harry is a progressive technologist and lawyer, with a background in
-          developing lightweight technological solutions for small to medium
-          campaigning organisations.
+        Edie Shepherd is a proud Wiradjuri and Noongar woman and senior organiser at Original Power. She has worked as a youth worker, community organiser and campaigner within Aboriginal and Torres Strait Islander communities, as well as in broader social and economic justice spaces.
         </p>
         <p>
-          Harry leads finance, governance, and fundraising for ProgTech, and
-          brings both expertise and passion for climate justice to the team.
+          Before Original Power, Edie spent 3 years working as an organiser in the trade union movement, running Aboriginal and Torres Strait Islander organising and political capacity building programs in Victoria.
         </p>
       </>
     ),
   },
   {
-    url: "/images/team-moira.jpg",
+    url: "/images/team-moira.png",
     name: "Moira Cully",
     bio: (
       <>
         <p>
           Moira is a data and digital organiser at Vic Trades Hall specialising
-          in supporting unions in best practice digital campaigning. 
+          in supporting unions in best practice digital campaigning.
         </p>
         <p>
         She leads one of the most effective technological
@@ -67,64 +60,61 @@ const list = [
     ),
   },
   {
-    url: "/images/team-edie.jpg",
-    name: "Edie Shepherd",
+    url: "/images/team-sofia.png",
+    name: "Sofia Madden",
     bio: (
       <>
         <p>
-          Edie Shepherd is a proud Wiradjuri and Noongar woman and senior
-          organiser at Original Power. She has worked as a youth worker,
-          community organiser and campaigner within Aboriginal and Torres Strait
-          Islander communities, as well as in broader social and economic
-          justice spaces.
+          Using great stories, technology and targeted data, Sofia has established and run strategic and creative businesses for impact -- most recently (since 2014) as the Co-Founder and Managing Director of Principle Co., an agency dedicated to social change and engaging people with causes and purposes.
         </p>
         <p>
-          Before OP, Edie has spent 3 years working as an organiser in the trade
-          union movement, running Aboriginal and Torres Strait Islander
-          organising and political capacity building programs in Victoria.
+          Her clients over the last 10 years include leaders of Australia’s civil society, politics and Government, as well as corporate, sporting, and cultural organisations. Sofia has worked with these organisations, campaigns and individuals to use their position in society to create positive social change in-line with their values which have culminated in some of the largest social movements of the decade such as Marriage Equality for Australia.
+        </p>
+        <p>
+        Sofia is an award-winning impact documentary producer, has worked in television, and holds a Bachelor of Communications from Western Sydney University.
         </p>
       </>
     ),
   },
   {
-    url: "/images/team-paul.jpg",
-    name: "Paul Day",
+    url: "/images/team-tom.png",
+    name: "Tom Sulston",
     bio: (
       <>
         <p>
-          Paul Day is a full stack developer specialising in progressive
-          technology in Australia. He brings his expertise in building
-          best-in-class tools that make digital fundraising accessible to
-          grassroots groups and small organisations through his work at Chuffed
-          and Raisely.
+          Tom is a principal consultant at ThoughtWorks, helping not-for-profit clients run software delivery projects. He’s also a board member of Digital Rights Watch and volunteers supporting activists create secure and robust systems.
         </p>
         <p>
-          Paul focuses on identifying areas where shared challenges offer
-          potential for building shared solutions - alongside running solidarity
-          tech support and training. He's excited to grow the strong progressive
-          tech community to further support organisations in doing good work.
+          At ProgTech, Tom is piloting programs to channel private sector tech expertise into projects for organisations lead by people of colour, First Nations people, people with disabilities and queer and trans people.
         </p>
       </>
     ),
   },
   {
-    url: "/images/team-sara.jpg",
-    name: "Sara Smylie",
+    url: "/images/team-elise.png",
+    name: "Elise Dalley",
     bio: (
       <>
         <p>
-          Sara is a National Coordinator of New Organising at United Workers
-          Union. United Workers Union members - including cleaners, early
-          childhood educators and care workers - are at the coalface of rising
-          inequality and changes to the nature of work, and are at the forefront
-          of fighting back.
+          Elise is Impact Director at Australian Progress, leading projects including Progress Labs and the Economic Media Centre. Through Progress Labs, Elise has incubated 17 new advocacy organisations and continues to build their capacity for impact as a mentor, trainer and fundraising strategist.
         </p>
         <p>
-          {" "}
-          Sara trials new approaches to organising, using tech to allow workers
-          in disparate sites to connect, take action and build power
-          collectively.
+          Elise brings her expertise in managing startups to fruition, alongside strong networks in the Australian progressive movement and a deep understanding of what the movement ecosystem needs to thrive.
         </p>
+      </>
+    ),
+  },
+  {
+    url: "/images/team-bridger.png",
+    name: "Bridger Rossiter",
+    bio: (
+      <>
+        <p>
+          Bridger brings extensive experience, working for 6 years as GetUp’s Digital Director alongside inspiration drawn from working on Bernie Sanders’ tech team to the Network Board.
+          </p>
+        <p>
+          He’s driven by a vision of powerful movement scale tools possible only through strategic collaboration - and his contribution to the ProgTech lies in mapping shared need across the sector to pitch projects with widespread and scalable impact potential.
+          </p>
       </>
     ),
   },
