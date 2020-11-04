643b46f60e919aef18190f1a2bc52981321aa40c
mkcully <mcully@vthc.org.au>
2020-11-04 11:01:54 +1100

    Update overview.js

--- pages/overview.js
+++ pages/overview.js
@@ -18,31 +18,25 @@ const Description = () => (
   <Box width={[1, 1 / 2]} pr={[0, 5]}>
     <Heading variant="highlight.pink">Who are we?</Heading>
     <p>
-      The Progressive Tech Network is a growing collective of progressive organisations
-      from across our movements teaming up on tech - including unions, not for
-      profits, enviro groups, and community organisations.
+      The Progressive Tech Network is building a world progressive organisations and activists have the best tools possible, and the skills and support to use them to win.
     </p>
 
     <p>
-      ProgTech is in its founding stages - and so focussing on learning about our shared needs,
-      and the key interventions we can make through collaboration. When the network launches in late
-      August, members will have access to a library of shared tools, resources and code; training and
-      mentorship programs; and a networked community who work together on solutions to shared challenges.
+    To achieve this, we're  bringing together a national collective of nonprofits, campaigning organisations, unions, vendors, agencies, and grassroots groups to build a thriving community, create accessible pathways to tech, widen access to innovative tech and drive collaboration across the sector.
     </p>
+  <p>
+  ProgTech is in its founding stages with a key focus on learning about the shared needs of the sector, developing the key interventions we can make through collaboration and expanding our membership network on a national scale to amplify our impact.
+  </p>
+  <p>ProgTech is incubated by the Centre for Australian Progress.</p>
 
 
-    <p style={{ marginBottom }}>
-      Our founding team is a group of people working across the progressive
-      sector - Edie Shepherd (Original Power); Sara Smylie (UWU); Clodagh Schofield (AYCC); Moira Cully (VTHC);
-      Harry Cossar-Gilbert (independant progressive developer); Paul Day (Raisely).
-    </p>
 
     <Heading variant="highlight.green">Why do we exist?</Heading>
     <p>
       It’s hard enough to win progressive change. It’s harder when our tech doesn’t work for us, and when we’re trying to solve our common challenges in isolation.
     </p>
     <p>
-    We’ve just run the biggest ever tech consultation with the progressive sector in Australia. Our results, plus meetings with key stakeholders and our own experience, tell us strongly that the progressive sector is lagging behind.
+    In 2020, we ran a tech consultation with more than 50 organisations across the progressive sector in Australia. Our results, plus meetings with key stakeholders and our own experience, tell us strongly that the progressive sector is lagging behind.
     </p>
     <p>
     Organisations report that they’re constantly reinventing the wheel, operating in silos, using inadequate tools with insufficient skills, making do with minimal investment, that the tech space is exclusionary, that tech staff are often unsupported, and that we’re not doing the long term movement wide thinking on tech that we need.
@@ -69,27 +63,7 @@ const Description = () => (
       and the technologists and vendors that support us - apolitical businesses contribute great things,
       but aren't a fit for this network. ProgTech is also not a network for political parties.</p>
 
-    <Heading variant="highlight.orange">What have we done to date? </Heading>
-    <p >
-      After listening, testing and incubating, ProgTech is now preparing for launch.
-      Consultation through sessions at FWD+Organise 2019 and the largest sector consultation
-      on tech informed key interventions the network has tested.
-    </p>
-    <p>
-      Mentorship pilots ran in the first half of 2020 - pairing a cohort of women, non-binary
-      and First Nations campaigners, organisers, and communcations mentees with amazing
-      progressive technologists. Skills based trainings were trialled and refined - focussing on
-      React and SQL, which were identified as key areas organisations wanted to see staff cultivate
-      skills in.
-    </p>
 
-<p style={{ marginBottom }}>
-      Before launching in late August, ProgTech is focussing on consulting with the organisations
-      and activists at the forefront of progressive change - first nations and people of colour led
-      groups - to ensure the network is grounded from the start in supporting this vital work. We're developing
-      structured mentorships, and technical volunteering programs to bring in expertise from inside and
-      outside the sector to add extra support without the extra hassle.
-    </p>
   </Box>
 );
 
