87e11d969e8559907a1fb059f18072c646586a94
mkcully <mcully@vthc.org.au>
2020-11-04 11:15:53 +1100

    Removing Zapier trainings

--- pages/trainings.js
+++ pages/trainings.js
@@ -10,12 +10,7 @@ import JoinNewsletter from "../components/JoinNewsletter";
 const Description = () => (
   <Flex flexDirection="column" width={[1, 1 / 2]} pr={[0, 5]}>
     <p>We're building skilled tech capacity for organisations across the progressive movement. RSVP here for upcoming trainings, including: </p>
-    <Heading variant="highlight.pink">Data automations with Zapier & custom webhooks - run by Code Nation</Heading>
-    <p><strong>Session 1: Getting started with Zapier, Thursday 22 October 11am - 12.30pm AEDT</strong></p>
-    <p><strong>Session 2: Zapier automations using webhooks and custom API requests, Thursday 29 October 11am - 12.30pm AEDT</strong></p>
-    <p>Many organisations are using out-of-the-box tools that don't integrate well. It's frustrating working with siloed data and having to repeat processes that could be automated. This training course will run over 2 sessions. The first session is an intro to using Zapier to connect commonly-used platforms, and the second session will teach you how to use webhooks and custom API requests in Zapier for custom integrations.</p>
-    <p><Link href="https://progtech.typeform.com/to/dYZWQJEJ#source=progtech-website" target="_blank" variant="buttons.primary">RSVP Here</Link></p>
-    <Heading mt={4} variant="highlight.pink">Intro to data science</Heading>
+        <Heading mt={4} variant="highlight.pink">Intro to data science</Heading>
     <p><strong>Wednesday 4 November, 5-6pm AEDT</strong></p>
     <p>Everyone is excited about data science but a lot of orgs aren’t in a position to take advantage of it. This session will introduce data science concepts for campaigning, and start a convo about the kinds of data science projects we might like to work on collectively.</p>
     <p><Link href="https://progtech.typeform.com/to/WPzYDrtD#source=progtech-website" target="_blank" variant="buttons.primary">RSVP Here</Link></p>
