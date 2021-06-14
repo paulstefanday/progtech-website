34cdc02dc2246858a1f5cb2e5dcc03054788a9db
leebagzlee <85263084+leebagzlee@users.noreply.github.com>
2021-06-14 10:46:11 +1000

    Update projects.js

--- pages/projects.js
+++ pages/projects.js
@@ -63,16 +63,16 @@ const Description = () => (
           <p>
             We’re working with eight campaigning organisations to determine key opportunities for product and tech collaboration. This piece of research focuses on campaign technology, organising technology and data solutions required leading up to the Federal Election.    
           </p>
-        
-        <Heading variant="highlight.orange">Tech Help Please</Heading>
-         <p>
-            We’re setting up a monthly peer to peer coaching day for Progressive Tech Members to access peer to peer learning and technical volunteers. If you’re interested in becoming a coach contact Hunter Lee at lee@australinprogress.org.au 
-         </p>
-
+         
          <Heading variant="highlight.pink">Progressive Tech Talks & Drinks</Heading>
          <p>
             Progresssive Tech Talks & Drinks are bi-monthly in-person events in Sydney and Melbourne to build community connections, share knowledge and seed collaborations. If you’re outside these cities and would like to start one up contact Hunter Lee at lee@australianprogress.org.au 
-          </p>
+          </p>        
+
+         <Heading variant="highlight.orange">Tech Help Please</Heading>
+         <p>
+            We’re setting up a monthly peer to peer coaching day for Progressive Tech Members to access peer to peer learning and technical volunteers. If you’re interested in becoming a coach contact Hunter Lee at lee@australinprogress.org.au 
+         </p>
 
          <Heading variant="highlight.orange">Movement Tech Product Scoping Paper</Heading>
          <p>
