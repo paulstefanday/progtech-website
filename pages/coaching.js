308c777ad5f42ca62862a9216e6b78244f69a76c
mkcully <mcully@vthc.org.au>
2021-07-13 17:12:46 +1000

    Update coaching.js

--- pages/coaching.js
+++ pages/coaching.js
@@ -39,7 +39,7 @@ const App = (props) => {
         <Container maxWidth={1200}>
           <Description />
           <Box width={[1, 1 / 2]} flexWrap="wrap">
-          
+          <p><script src="https://static.airtable.com/js/embed/embed_snippet_v1.js"></script><iframe class="airtable-embed airtable-dynamic-height" src="https://airtable.com/embed/shr7QJENmCKms2hBE?backgroundColor=blue" frameborder="0" onmousewheel="" width="100%" height="2584" style="background: transparent; border: 1px solid #ccc;"></iframe></p>
           </Box>
         </Container>
       </Box>
