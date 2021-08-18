cd175ea00d4a83a48c7f7276765fc41f43e989b3
leebagzlee <85263084+leebagzlee@users.noreply.github.com>
2021-08-18 16:15:44 +0930

    Update trainings.js
    Text changes

--- pages/trainings.js
+++ pages/trainings.js
@@ -11,17 +11,18 @@ const Description = () => (
   <Flex flexDirection="column" width={[1, 1 / 2]} pr={[0, 5]}>
     <p>We're building skilled tech capacity for organisations across the progressive movement. RSVP here for upcoming trainings, including: </p>
 
-    <Heading mt={4} variant="highlight.pink">Intermediate Google Analytics and Data Studio Training</Heading>
-    <p><strong>Friday July 30th, Online</strong></p>
-    <p>One-Day intermediate training in Google Analytics and Data Studio. Participants will learn how to build Google Analytics Custom Segments and build reports in Google Data Studio. Trainers: Aaron Lamb, Director of Fundraising, Marketing and Communications at Environmental Defenders Office, Teagan Buckley (she/her) Digital Marketer at United Workers Union and Amelia Briggs Data Organiser at United Workers Union. </p>
+    <Heading mt={4} variant="highlight.pink">Digital Strategy - Supporter Journey Mapping Bootcamp </Heading>
+    <p><strong>Mon Oct 4th - Oct 26th Online</strong></p>
+    <p> Digital Strategy: Supporter Journey Mapping Bootcamp  is a new training for people from non-profit organisations and grassroots groups to practically understand the fundamentals of digital strategy, develop skills in supporter journey mapping and implement automated supporter journeys on common toolsets.
+Over four half-days in October, the program will coach participants to develop a basic digital strategy, design supporter journeys and implement these journeys on common tools. </p>
     <p>
-         <Link href="http://eventbrite.com.au/e/intermediate-google-analytics-and-google-data-studio-training-tickets-159760972369"><Button variant="primary">Find out more</Button></Link>
+         <Link href="https://www.eventbrite.com.au/e/digital-strategy-supporter-journey-mapping-bootcamp-tickets-166479004183"><Button variant="primary">Find out more</Button></Link>
     </p>
     <Heading mt={4} variant="highlight.orange"> Movement Tech Product Scoping Paper Briefing </Heading>
-    <p><strong>Tuesday, July 6th, Online</strong></p>
+    <p><strong>Tue, 31 Aug 2021 12:00 PM </strong></p>
     <p>Find out more about our initial findings scoping movement-wide campaigning and organising product solutions. This research paper worked with eight progressive campaigning organisations to explore product solutions for campaign technology, organising technology and data solutions leading up to the Federal Election.</p>  
      <p>
-         <Link href="https://www.eventbrite.com.au/e/progressive-tech-product-scoping-briefing-tickets-159923043127"><Button variant="primary">Register Now</Button></Link>
+         <Link href="https://www.eventbrite.com.au/e/progressive-tech-product-scoping-briefing-tickets-162385203515"><Button variant="primary">Register Now</Button></Link>
     </p>
 
     <p>More coming soon! Please note that these trainings are for the volunteers and staff of network members only. If there's a training that would help you, find out more about how your organisation can join <a href="https://progtech.net.au/join">here</a>. If you need our help to speak to your manager about why joining the Progressive Tech Network is a good idea, get in touch at <a href="mailto:hello@progtech.net.au">hello@progtech.net.au</a></p>
